bb17114071a02a511fbcdad49b25b2dc9d190781
eduardorocco <eduardo_rocco@hotmail.it>
2024-10-15 16:46:05 +0200

    ADD final code

--- app.js
+++ app.js
@@ -6,21 +6,30 @@
 // numero % 3/5
 
 // PER ogni interazione stampare il numero di interazioni 
+// SE il numero di interazione è divisibile per 3 && 5 scrivi fizz buzz
 // SE il numero di interazione è divisivile per 3 scrivi fizz
 // SE il numero di interazioni è divisible per 5 scrivi buzz
-// SE il numero di interazione è divisibile per 3 && 5 scrivi fizz buzz
+
 
 for (let i = 0; i < 100; i++) {
 
     let num = i + 1
 
-    if (num % 3 == 0) {
-        console.log('Fizz')
+    if (num % 3 == 0 && num % 5 == 0) {
+        console.log('FizzBuzz')
     } 
     
-    else if  (num % 5 == 0)
+    else if  (num % 5 == 0) {
         console.log('Buzz') 
-    
+    }
 
-    
+    else if (num % 3 == 0) {
+        console.log('Fizz')
+    }
+
+    else {
+        console.log(num)
+    }
+
+ 
 }
