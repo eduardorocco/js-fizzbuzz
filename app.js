8bbd7b1175ee63e819eb4ab1a84c3de8a9fe98f6
eduardorocco <eduardo_rocco@hotmail.it>
2024-10-15 16:25:19 +0200

    pseudo code

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('ciao')
+// programma da 1 a 100 numeri
+// multipli 3 FIZZ
+// multipli 5 BUZZ
+//multipli 3/5 FIZZBUZZ
+
+// numero % 3/5
+
+// PER ogni interazione stampare il numero di interazioni 
+// SE il numero di interazione è divisivile per 3 scrivi fizz
+// SE il numero di interazioni è divisible per 5 scrivi buzz
+// SE il numero di interazione è divisibile per 3 && 5 scrivi fizz buzz
+
